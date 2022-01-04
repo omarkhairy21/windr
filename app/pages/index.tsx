0ab7b46e89d9c5a8aaf680aefc365776b8ee8f18
Omar Khairy <omarkhairy21@gmail.com>
2022-01-04 02:31:00 +0200

    Add title and favicon

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import { LandingPageHeader } from '@components/Headers/LandingPageHeader'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Windr - Alpha Version </title>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
       <LandingPageHeader />
     </>
   )
