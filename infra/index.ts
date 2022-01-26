cc484edad82a522637dd1b525ee6c98ce9bd72ad
Omar Khairy <omarkhairy21@gmail.com>
2022-01-26 17:58:52 +0200

    Try Build Image and push to ecr

--- infra/index.ts
+++ infra/index.ts
@@ -1,42 +1,52 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import { getEBConfig } from "./utils";
 
-import { AccessKey as SetupECRAccessKey,  SecretKey as SetupECRSecretKey } from './setup-ecr';
+const region = pulumi.output(aws.getRegion().then(data => data.name));
+const accountId = pulumi.output(aws.getCallerIdentity().then(data => data.accountId));
+const appName = 'windr'
 
-const config = new pulumi.Config();
-const appName = config.require("appName");
-const appEnvironment = config.require("appEnvironment");
 
-/* 
-  1- Elastic Beanstalk service role
-     https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts-roles-service.html
-  
-  2- Elastic Beanstalk instance profile
-     https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts-roles-instance.html
+// Default vpc and subnets
+// const defaultDefaultVpc = new aws.ec2.DefaultVpc("default-vpc", {
+//   tags: {
+//       Name: "Default VPC",
+//   },
+// });
 
-  3- Managing Elastic Beanstalk service roles
-      https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/iam-servicerole.html
-
-  4- Service roles, instance profiles, and user policies
-    https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/concepts-roles.html
-
-  Use Docker image from ECR repository
-  https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create_deploy_docker.container.console.html
-
-  we need to give access to ec2 instance role to read the image from ECR repository
-  */
-// (1) S3
-export const bucket = new aws.s3.Bucket("my-bucket");
+// new aws.iam.User('Github-Actions-Allow-Manage-ECR')
+// aws.iam.getPolicy({
+//   name: 'Github-Actions-Allow-Manage-ECR-Policy'
+// })
 
 // (2) ECR
-export const ecr = new aws.ecr.Repository(`${appName}`, {
-  name: `${appName}`
-});
+const ecr = new aws.ecr.Repository(`${appName}`, {
+  name: `${appName}`,
+  tags: {
+    Name: 'windr-ebs-deployment-production',
+  },
+})
+
+const imageUrl = pulumi.interpolate `${accountId}.dkr.ecr.${region}.amazonaws.com/${ecr.name}:latest`
+
+
+// (1) S3
+  const deploymentBucket = new aws.s3.Bucket("windr-ebs-deployment", {})
+  const deploymentObject = pulumi.all([imageUrl]).apply(([imageUrl]) => {
+    const json = JSON.stringify(getEBConfig(imageUrl));
+    return new aws.s3.BucketObject("windr-ebs-deployment-object", {
+      bucket: deploymentBucket.id,
+      key: "Dockerrun.aws.json",
+      contentType: "application/json",
+      source: new pulumi.asset.StringAsset(json),
+    })
+
+  })  
 
 // (3-1) Setup IamInstanceProfile
-export const instanceProfileRole = new aws.iam.Role(`${appName}-eb-ec2-role`, {
-  name: `${appName}-eb-ec2-role`,
+const instanceProfileRole = new aws.iam.Role(`${appName}-ebs-ec2-role`, {
+  name: `${appName}-ebs-ec2-role`,
   description: "Role for EC2 managed by EB",
   assumeRolePolicy: JSON.stringify({
     Version: "2012-10-17",
@@ -53,3 +63,58 @@ export const instanceProfileRole = new aws.iam.Role(`${appName}-eb-ec2-role`, {
   })
 });
 
+const instanceProfile = new aws.iam.InstanceProfile(`${appName}-ebs-instance-profile`, {
+  name: `${appName}-ebs-instance-profile`,
+  role: instanceProfileRole.id
+})
+
+// 4 Create new PostgresDb instance
+const db = new aws.rds.Instance(`${appName}-db`, {
+  engine: "postgres",
+  engineVersion: "13.4",
+  instanceClass: "db.t4g.micro",
+  allocatedStorage: 10,
+  username: "postgres",
+  password: "postgres",
+});
+
+const { dbUsername, dbPassword } = pulumi.all([db.username, db.password])
+  .apply(([dbUsername, dbPassword]) => ({dbUsername, dbPassword: dbPassword || '' }))
+
+// 5 Create new Elastic Beanstalk Application
+const ebApp = new aws.elasticbeanstalk.Application(`${appName}-ebs-app`, {
+  name: `${appName}-ebs-app`,
+  description: "Production Application for Windr",
+});
+
+const appVersion = new aws.elasticbeanstalk.ApplicationVersion('default', {
+  application: ebApp.name,
+  description: "Production Version 1.0",
+  bucket: deploymentBucket.id,
+  key: deploymentObject.key,
+});
+
+// 6 Create new Elastic Beanstalk Environment
+const ebEnv = new aws.elasticbeanstalk.Environment(`${appName}-ebs-env`, {
+  application: ebApp.name,
+  version: appVersion,
+  solutionStackName: "64bit Amazon Linux 2 v3.4.10 running Docker",
+  settings: [
+    {
+      name: "IamInstanceProfile",
+      namespace: "aws:autoscaling:launchconfiguration",
+      value: instanceProfile.id,
+    },
+    {
+      name: `DATABASE_USERNAME`,
+      namespace: "aws:elasticbeanstalk:application:environment",
+      value: dbUsername,
+    },{
+      name: `DATABASE_PASSWORD`,
+      namespace: "aws:elasticbeanstalk:application:environment",
+      value: dbPassword,
+    },
+  ]
+});
+
+export const endpoint = ebEnv.endpointUrl
---
+++ infra/utils.ts
@@ -0,0 +1,15 @@
+import { Output } from "@pulumi/pulumi";
+
+
+export const getEBConfig = (imageUrl: string) => ({
+  AWSEBDockerrunVersion: '1',
+  Image: {
+    Name : imageUrl,
+    Update: true
+  },
+  Ports: [
+    {
+      ContainerPort: "1337"
+    }
+  ]
+})
