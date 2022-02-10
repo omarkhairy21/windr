77b7397488afcb6212d7a700af5e97655a63619b
Omar Khairy <omarkhairy21@gmail.com>
2022-02-10 16:03:34 +0200

    Pass Props to Layout in settings page

--- app/pages/settings.tsx
+++ app/pages/settings.tsx
@@ -1,10 +1,18 @@
-import { Box, Heading } from '@chakra-ui/layout'
+import { Box, Heading, useDisclosure } from '@chakra-ui/layout'
 import { Layout } from '@components/Layout'
 
 export default function Dashboard() {
+  const {
+    isOpen: isNavItemsDrawerOpen,
+    onOpen: onOpenNavItemsDrawer,
+    onClose: onCloseNavItemsDrawer,
+  } = useDisclosure()
   return (
     <>
-      <Layout>
+      <Layout
+        onOpenNavItemsDrawer={onOpenNavItemsDrawer}
+        onCloseNavItemsDrawer={onCloseNavItemsDrawer}
+        isNavItemsDrawerOpen={isNavItemsDrawerOpen}>
         <Box>
           <Heading>There will be Setting Here</Heading>
         </Box>
